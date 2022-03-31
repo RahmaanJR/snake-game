b4a3443a6ec0d852a67a7c6736cf5d2291d009e0
RahmaanJR <abdulrahmaanrufai@cmail.carleton.ca>
2022-03-31 15:16:52 -0400

    Classic nokia snake game made with python, playable in the. terminal window
    Created snake.py containing the code to run the snake game

---
+++ snake.py
@@ -0,0 +1,88 @@
+import curses
+from random import randint
+
+#setting up window "win"
+curses.initscr() #screen initialization
+win = curses.newwin(20, 60, 0, 0) #y coordinate, x coordinate
+win.keypad(1) #enabling keypad to control snake
+curses.noecho() #disabling other inputs
+curses.curs_set(0) 
+win.border(0)
+win.nodelay(1)
+
+
+#snake and food
+snake = [(4, 10), (4, 9), (4, 8)] #snake list is storing x & y coordinates which are tuples because they are immutable
+food = (10, 20)
+win.addch(food[0], food[1], '#') #add initial food character to window
+
+#game logic
+score = 0
+ESC = 27 #escape key is defined as key 27 in the curses module
+key = curses.KEY_RIGHT #snake begins motion to the right
+
+while key != ESC:
+
+    win.addstr(0, 2, 'Score ' +  str(score) + '')
+    win.timeout(150  - (len(snake)) // 5 + len(snake) // 10% 120) #increasing snake movement speed based on its length
+
+    prev_key = key
+    event = win.getch() #get next character
+    key = event if event != -1 else prev_key
+
+    if key not in [curses.KEY_LEFT, curses.KEY_UP, curses.KEY_RIGHT, curses.KEY_DOWN, ESC]:
+        key = prev_key
+
+    #calculate the next coordinates
+    y = snake[0][0]
+    x = snake[0][1]
+
+    if key == curses.KEY_DOWN:
+        y += 1
+
+    if key == curses.KEY_UP:
+        y -= 1
+
+    if key == curses.KEY_LEFT:
+        x -= 1
+
+    if key == curses.KEY_RIGHT:
+        x += 1
+
+    snake.insert(0, (y, x)) #append 0(n)
+
+    #check if we hit the border
+    if y == 0: 
+        break
+    if y == 19: #per window dimensions set with window initialization
+        break 
+    if x == 0: 
+        break
+    if x == 59: #per window dimensions set with window initialization
+        break 
+
+    #check if snake runs over itself
+    if snake[0] in snake[1:]: #if the head of the snake intersects with any other current position of the snake
+        break 
+
+    if snake[0] == food:
+        score += 1
+        food = ()
+
+        while food == ():
+            food = (randint(1, 18), randint(1, 58))
+
+            if food in snake:
+                food = ()
+
+        win.addch(food[0], food[1], '#')
+
+    else:
+        last = snake.pop()
+        win.addch(last[0], last[1], ' ')
+
+    win.addch(snake[0][0], snake[0][1], '*')
+
+
+curses.endwin()
+print(f"Final score = {score}")
